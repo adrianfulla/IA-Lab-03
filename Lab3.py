c6ed65b80aa395ebce98e4d46160a35a082f320c
Adrian Fulladolsa <ful21592@uvg.edu.gt>
2024-04-29 18:32:10 -0600

    Posterior fix

--- Lab3.py
+++ Lab3.py
@@ -10,7 +10,6 @@ prior = np.full((tablero, tablero), 1/(tablero**2))
 
 def update_probabilities(prior, evidence_position, evidence_color):
     likelihood = np.zeros_like(prior)
-    
     for i in range(tablero):
         for j in range(tablero):
             distance = np.abs(i - evidence_position[0]) + np.abs(j - evidence_position[1])
@@ -22,19 +21,21 @@ def update_probabilities(prior, evidence_position, evidence_color):
     posterior /= np.sum(posterior)
     return posterior
 
-def plot_distribution(probabilities, title='Probability Distribution'):
+def plot_distribution(probabilities, title='Distribucion de Probabilidades'):
     plt.figure(figsize=(8, 6))
     sns.heatmap(probabilities, annot=True, fmt=".2f", cmap="Blues", cbar=False)
     plt.title(title)
     plt.show()
 
 
-position = (1, 5)  
-color = 'R'         
-updated_probabilities = update_probabilities(prior, position, color)
+positions = [((1, 5), 'Y'),((2, 3), 'R'),((8, 8), 'O')]
+        
+for pos in positions:
+    prior = update_probabilities(prior, pos[0], pos[1])
 
-plot_distribution(updated_probabilities, 'Distribución de probabilidades')
+
+plot_distribution(prior, 'Distribución de probabilidades')
 
 
-localizacion_mas_probable = np.unravel_index(np.argmax(updated_probabilities), updated_probabilities.shape)
+localizacion_mas_probable = np.unravel_index(np.argmax(prior), prior.shape)
 print("La celda más probable donde se encuentra el fantasma es:", localizacion_mas_probable)
